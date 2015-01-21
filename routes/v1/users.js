ae06514a2ffa73f26cf5371ae7ffc90c93293f5d
Julian Lam <julian@designcreateplay.com>
2015-01-21 13:16:45 -0500

    re-ordering PUT route in users ns

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -2,13 +2,12 @@
 /* globals module, require */
 
 var Users = require.main.require('./src/user'),
-	// Meta = require.main.require('./src/meta'),
 	apiMiddleware = require('../../middleware'),
 	errorHandler = require('../../lib/errorHandler'),
 	utils = require('./utils');
 
 
-module.exports = function(middleware) {
+module.exports = function(/*middleware*/) {
 	var app = require('express').Router();
 
 	app.post('/', apiMiddleware.requireUser, apiMiddleware.requireAdmin, function(req, res) {
@@ -19,18 +18,17 @@ module.exports = function(middleware) {
 		// Create a new user
 	});
 
-	app.post('/:userslug/follow', apiMiddleware.requireUser, function(req, res) {
-		Users.getUidByUserslug(req.params.userslug, function(err, targetUid) {
-			console.log(targetUid);
-			Users.follow(req.user.uid, targetUid, function(err) {
-				return errorHandler.handle(err, res);
-			});
-		});
-	});
-
 	app.put('/', apiMiddleware.requireUser, function(req, res) {
 		Users.updateProfile(req.user.uid, req.body, function(err) {
 			return errorHandler.handle(err, res);
+		});
+	});
+
+	app.post('/:userslug/follow', apiMiddleware.requireUser, function(req, res) {
+		Users.getUidByUserslug(req.params.userslug, function(err, targetUid) {
+			Users.follow(req.user.uid, targetUid, function(err) {
+				return errorHandler.handle(err, res);
+			});
 		});
 	});
 
