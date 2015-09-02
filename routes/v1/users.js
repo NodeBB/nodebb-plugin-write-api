7a55d6ab8c4631198a601274027c0c22e81039f3
Julian Lam <julian@designcreateplay.com>
2015-09-02 11:45:14 -0400

    Close #34
    This commit exposes POST|DELETE /api/v1/users/:uid/ban to allow write API access to ban users.

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -91,6 +91,18 @@ module.exports = function(/*middleware*/) {
 			});
 		});
 
+	app.route('/:uid/ban')
+		.post(apiMiddleware.requireUser, apiMiddleware.exposeAdmin, function(req, res) {
+			Users.ban(req.params.uid, function(err) {
+				errorHandler.handle(err, res);
+			});
+		})
+		.delete(apiMiddleware.requireUser, apiMiddleware.exposeAdmin, function(req, res) {
+			Users.unban(req.params.uid, function(err) {
+				errorHandler.handle(err, res);
+			});
+		});
+
 	app.route('/:uid/tokens')
 		.get(apiMiddleware.requireUser, function(req, res) {
 			if (parseInt(req.params.uid, 10) !== parseInt(req.user.uid, 10)) {
