a8347458bfca7cdb4e067fcd4b179e535b59c0b4
SunDwarf <eyesismine@gmail.com>
2016-03-26 10:34:44 +0000

    Add post deleting. Closes #49

--- routes/v1/posts.js
+++ routes/v1/posts.js
@@ -10,27 +10,35 @@ var posts = require.main.require('./src/posts'),
 module.exports = function(middleware) {
 	var app = require('express').Router();
 
-	app.put('/:pid', apiMiddleware.requireUser, function(req, res) {
-		if (!utils.checkRequired(['content'], req, res)) {
-			return false;
-		}
+	app.route('/:pid')
+		.put(apiMiddleware.requireUser, function(req, res) {
+			if (!utils.checkRequired(['content'], req, res)) {
+				return false;
+			}
 
-		var payload = {
-			uid: req.user.uid,
-			pid: req.params.pid,
-			content: req.body.content,
-			options: {}
-		};
+			var payload = {
+				uid: req.user.uid,
+				pid: req.params.pid,
+				content: req.body.content,
+				options: {}
+			};
 
-		if (req.body.handle) { payload.handle = req.body.handle; }
-		if (req.body.title) { payload.title = req.body.title; }
-		if (req.body.topic_thumb) { payload.options.topic_thumb = req.body.topic_thumb; }
-		if (req.body.tags) { payload.options.tags = req.body.tags; }
+			if (req.body.handle) { payload.handle = req.body.handle; }
+			if (req.body.title) { payload.title = req.body.title; }
+			if (req.body.topic_thumb) { payload.options.topic_thumb = req.body.topic_thumb; }
+			if (req.body.tags) { payload.options.tags = req.body.tags; }
 
-		posts.edit(payload, function(err) {
-			errorHandler.handle(err, res);
+			posts.edit(payload, function(err) {
+				errorHandler.handle(err, res);
+			})
+		})
+		.delete(apiMiddleware.requireUser, function(req, res) {
+			posts.delete(req.params.pid, req.user.uid, function(err) {
+				errorHandler.handle(err, res);
+			});
 		});
-	});
+
+		
 
 	return app;
 };
