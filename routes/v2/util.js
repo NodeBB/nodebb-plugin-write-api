aafd2f6afb2f745cefda5574862ccf867a7983ba
Julian Lam <julian@nodebb.org>
2017-07-01 15:39:57 -0400

    fixed indentation in util.js

--- routes/v2/util.js
+++ routes/v2/util.js
@@ -1,5 +1,5 @@
 'use strict';
-/* globals module, require */
+/* global module, require */
 
 var apiMiddleware = require('./middleware');
 
@@ -8,14 +8,17 @@ var uploadController = require.main.require('./src/controllers/uploads');
 var meta = require.main.require('./src/meta');
 
 module.exports = function(/*middleware*/) {
-  var app = require('express').Router();
-  app.route('/upload').post(apiMiddleware.requireUser, multipart(), function(req, res, next){
-    uploadController.upload(req, res, function(uploadedFile, callback){
-      if(parseInt(meta.config.allowFileUploads) !== 1){
-        return callback(new Error('[[error:uploads-are-disabled]]'));
-      }
-      uploadController.uploadFile(req.user.uid, uploadedFile, callback);
-    }, next);
-  });
-  return app;
-}
+	var app = require('express').Router();
+
+	app.route('/upload').post(apiMiddleware.requireUser, multipart(), function(req, res, next) {
+		uploadController.upload(req, res, function(uploadedFile, callback) {
+			if (parseInt(meta.config.allowFileUploads) !== 1) {
+				return callback(new Error('[[error:uploads-are-disabled]]'));
+			}
+
+			uploadController.uploadFile(req.user.uid, uploadedFile, callback);
+		}, next);
+	});
+
+	return app;
+};
