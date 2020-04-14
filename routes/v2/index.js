8911b4031d6fb6c5516acd89167a9c1151af4051
Julian Lam <julian@nodebb.org>
2020-04-14 16:33:34 -0400

    feat: allow peer plugins to hook into write-api on a per-route basis
    fixes #121

--- routes/v2/index.js
+++ routes/v2/index.js
@@ -1,19 +1,31 @@
 'use strict';
-/* globals module, require */
 
-var apiMiddleware = require('./middleware'),
-	errorHandler = require('../../lib/errorHandler'),
-	plugins = require.main.require('./src/plugins'),
-	writeApi = module.parent.parent.exports;
+const apiMiddleware = require('./middleware');
+const utils = require('./utils');
+const errorHandler = require('../../lib/errorHandler');
+const plugins = require.main.require('./src/plugins');
+const writeApi = module.parent.parent.exports;
 
 module.exports = function (app, coreMiddleware) {
-	app.use(function(req, res, next) {
+	app.use(function (req, res, next) {
 		if (writeApi.settings.requireHttps === 'on' && req.protocol !== 'https') {
 			res.set('Upgrade', 'TLS/1.0, HTTP/1.1');
 			return errorHandler.respond(426, res);
-		} else {
-			next();
 		}
+		next();
+	});
+
+	app.use(async (req, res, next) => {
+		// Allow plugins to hook into arbitrary routes
+		await plugins.fireHook('response:plugin.write-api.route', {
+			req: req,
+			res: res,
+			next: next,
+			utils: utils,
+			errorHandler: errorHandler,
+			method: req.method,
+			route: req.originalUrl,
+		});
 	});
 
 	app.use('/users', require('./users')(coreMiddleware));
@@ -23,21 +35,21 @@ module.exports = function (app, coreMiddleware) {
 	app.use('/categories', require('./categories')(coreMiddleware));
 	app.use('/util', require('./util')(coreMiddleware));
 
-	app.get('/ping', function(req, res) {
+	app.get('/ping', function (req, res) {
 		res.status(200).json({
 			code: 'ok',
 			message: 'pong',
-			params: {}
+			params: {},
 		});
 	});
 
-	app.post('/ping', apiMiddleware.requireUser, function(req, res) {
+	app.post('/ping', apiMiddleware.requireUser, function (req, res) {
 		res.status(200).json({
 			code: 'ok',
 			message: 'pong, accepted test POST ping for uid ' + req.user.uid,
 			params: {
-				uid: req.user.uid
-			}
+				uid: req.user.uid,
+			},
 		});
 	});
 
@@ -47,11 +59,15 @@ module.exports = function (app, coreMiddleware) {
 		router: customRouter,
 		apiMiddleware: apiMiddleware,
 		middleware: coreMiddleware,
-		errorHandler: errorHandler
+		errorHandler: errorHandler,
 	}, function (err, payload) {
+		if (err) {
+			// ¯\_(ツ)_/¯
+		}
+
 		app.use('/', payload.router);
 
-		app.use(function(req, res) {
+		app.use(function (req, res) {
 			// Catch-all
 			errorHandler.respond(404, res);
 		});
