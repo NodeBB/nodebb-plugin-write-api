c9daf85cb01b048b1b6321c928e2e744f267b18c
Julian Lam <julian@nodebb.org>
2020-04-24 10:17:10 -0400

    fix(style): linted v1/topics.js

--- routes/v1/topics.js
+++ routes/v1/topics.js
@@ -1,62 +1,61 @@
 'use strict';
-/* globals module, require */
 
-var Topics = require.main.require('./src/topics'),
-	Posts = require.main.require('./src/posts'),
-	apiMiddleware = require('./middleware'),
-	errorHandler = require('../../lib/errorHandler'),
-	utils = require('./utils'),
-	winston = require.main.require('winston');
+var Topics = require.main.require('./src/topics');
+var Posts = require.main.require('./src/posts');
+var apiMiddleware = require('./middleware');
+var errorHandler = require('../../lib/errorHandler');
+var utils = require('./utils');
+var winston = require.main.require('winston');
 
-module.exports = function(middleware) {
+module.exports = function () {
 	var app = require('express').Router();
 
 	app.route('/')
-		.post(apiMiddleware.requireUser, function(req, res) {
+		.post(apiMiddleware.requireUser, function (req, res) {
 			if (!utils.checkRequired(['cid', 'title', 'content'], req, res)) {
 				return false;
 			}
 
 			var payload = {
-					cid: req.body.cid,
-					title: req.body.title,
-					content: req.body.content,
-					tags: req.body.tags || [],
-					uid: req.user.uid,
-					timestamp: req.body.timestamp
-				};
+				cid: req.body.cid,
+				title: req.body.title,
+				content: req.body.content,
+				tags: req.body.tags || [],
+				uid: req.user.uid,
+				timestamp: req.body.timestamp,
+			};
 
-			Topics.post(payload, function(err, data) {
+			Topics.post(payload, function (err, data) {
 				return errorHandler.handle(err, res, data);
 			});
 		});
 
 	app.route('/:tid')
-		.post(apiMiddleware.requireUser, apiMiddleware.validateTid, function(req, res) {
+		.post(apiMiddleware.requireUser, apiMiddleware.validateTid, function (req, res) {
 			if (!utils.checkRequired(['content'], req, res)) {
 				return false;
 			}
 
 			var payload = {
-					tid: req.params.tid,
-					uid: req.user.uid,
-					req: req,	// For IP recording
-					content: req.body.content,
-					timestamp: req.body.timestamp
-				};
+				tid: req.params.tid,
+				uid: req.user.uid,
+				req: req,	// For IP recording
+				content: req.body.content,
+				timestamp: req.body.timestamp,
+			};
 
 			if (req.body.toPid) { payload.toPid = req.body.toPid; }
 
-			Topics.reply(payload, function(err, returnData) {
+			Topics.reply(payload, function (err, returnData) {
 				errorHandler.handle(err, res, returnData);
 			});
 		})
-		.delete(apiMiddleware.requireUser, apiMiddleware.validateTid, function(req, res) {
-			Topics.delete(req.params.tid, req.params._uid, function(err) {
+		.delete(apiMiddleware.requireUser, apiMiddleware.validateTid, function (req, res) {
+			Topics.delete(req.params.tid, req.params._uid, function (err) {
 				errorHandler.handle(err, res);
 			});
 		})
-		.put(apiMiddleware.requireUser, function(req, res) {
+		.put(apiMiddleware.requireUser, function (req, res) {
 			if (!utils.checkRequired(['pid', 'content'], req, res)) {
 				return false;
 			}
@@ -65,7 +64,7 @@ module.exports = function(middleware) {
 				uid: req.user.uid,
 				pid: req.body.pid,
 				content: req.body.content,
-				options: {}
+				options: {},
 			};
 			console.log(payload);
 
@@ -75,58 +74,58 @@ module.exports = function(middleware) {
 			if (req.body.topic_thumb) { payload.options.topic_thumb = req.body.topic_thumb; }
 			if (req.body.tags) { payload.options.tags = req.body.tags; }
 
-			Posts.edit(payload, function(err, returnData) {
+			Posts.edit(payload, function (err, returnData) {
 				errorHandler.handle(err, res, returnData);
 			});
 		});
 
 	app.route('/:tid/follow')
-		.post(apiMiddleware.requireUser, apiMiddleware.validateTid, function(req, res) {
-			Topics.follow(req.params.tid, req.user.uid, function(err) {
+		.post(apiMiddleware.requireUser, apiMiddleware.validateTid, function (req, res) {
+			Topics.follow(req.params.tid, req.user.uid, function (err) {
 				errorHandler.handle(err, res);
 			});
 		})
-		.delete(apiMiddleware.requireUser, apiMiddleware.validateTid, function(req, res) {
-			Topics.unfollow(req.params.tid, req.user.uid, function(err) {
+		.delete(apiMiddleware.requireUser, apiMiddleware.validateTid, function (req, res) {
+			Topics.unfollow(req.params.tid, req.user.uid, function (err) {
 				errorHandler.handle(err, res);
 			});
 		});
 
 	app.route('/:tid/tags')
-		.post(apiMiddleware.requireUser, apiMiddleware.validateTid, function(req, res) {
+		.post(apiMiddleware.requireUser, apiMiddleware.validateTid, function (req, res) {
 			if (!utils.checkRequired(['tags'], req, res)) {
 				return false;
 			}
 
-			Topics.updateTags(req.params.tid, req.body.tags, function(err) {
+			Topics.updateTags(req.params.tid, req.body.tags, function (err) {
 				errorHandler.handle(err, res);
 			});
 		})
-		.delete(apiMiddleware.requireUser, apiMiddleware.validateTid, function(req, res) {
-			Topics.deleteTopicTags(req.params.tid, function(err) {
+		.delete(apiMiddleware.requireUser, apiMiddleware.validateTid, function (req, res) {
+			Topics.deleteTopicTags(req.params.tid, function (err) {
 				errorHandler.handle(err, res);
 			});
 		});
 
 	// **DEPRECATED** Do not use.
 	app.route('/follow')
-		.post(apiMiddleware.requireUser, function(req, res) {
+		.post(apiMiddleware.requireUser, function (req, res) {
 			winston.warn('[write-api] /api/v1/topics/follow route has been deprecated, please use /api/v1/topics/:tid/follow instead.');
 			if (!utils.checkRequired(['tid'], req, res)) {
 				return false;
 			}
 
-			Topics.follow(req.body.tid, req.user.uid, function(err) {
+			Topics.follow(req.body.tid, req.user.uid, function (err) {
 				errorHandler.handle(err, res);
 			});
 		})
-		.delete(apiMiddleware.requireUser, function(req, res) {
+		.delete(apiMiddleware.requireUser, function (req, res) {
 			winston.warn('[write-api] /api/v1/topics/follow route has been deprecated, please use /api/v1/topics/:tid/follow instead.');
 			if (!utils.checkRequired(['tid'], req, res)) {
 				return false;
 			}
 
-			Topics.unfollow(req.body.tid, req.user.uid, function(err) {
+			Topics.unfollow(req.body.tid, req.user.uid, function (err) {
 				errorHandler.handle(err, res);
 			});
 		});
